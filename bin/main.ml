426ce7404b87b615e455b0c1d36b9ca6033caa24
Arnab Ray <18110248+arnab-ray@users.noreply.github.com>
2024-12-02 23:55:23 +0530

    save changes

--- bin/main.ml
+++ bin/main.ml
@@ -6,3 +6,7 @@ let test_1_2 = Aoc.Problem_1_2.similarity_score Aoc.Problem_1_2.input
 let () = 
   Printf.printf "similarity score: %i\n%!" test_1_2
   (** print_endline "Hello, World!" *)
+
+let test_2 = Aoc.Day02.count_safe_reports (Aoc.Day02.is_safe true) Aoc.Day02.input
+let () = 
+  Printf.printf "safe count: %i\n%!" test_2
--- test/test.ml
+++ test/test.ml
@@ -4,6 +4,8 @@ open Aoc
 let tests = "test suite for aoc day 01" >::: [
   "total_distance" >:: (fun _ -> assert_equal 3574690 (Problem_1_1.total_distance Problem_1_1.input));
   "similarity_score" >:: (fun _ -> assert_equal 22565391 (Problem_1_2.similarity_score Problem_1_2.input));
+  "safe count" >:: (fun _ -> assert_equal 341 (Day02.count_safe_reports (Day02.is_safe false) Day02.input));
+  "safe count demented" >:: (fun _ -> assert_equal 404 (Day02.count_safe_reports (Day02.is_safe true) Day02.input))
 ]
 
 let _ = run_test_tt_main tests
